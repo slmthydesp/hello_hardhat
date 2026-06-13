f6784fd4a96581c2daa67d16c61498bf37424842
lirui311012 <lirui311012@gmail.com>
2026-06-13 15:26:58 +0800

    fix: deploy Counter contract

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hello_hardhat
+
+1. npm install -D @openzeppelin/contracts
+2. npx hardhat compile
+3. npx hardhat test
+4. npx hardhat ignition deploy ignition/modules/Counter.ts --network localhost
+5. npx hardhat ignition deploy ignition/modules/Counter.ts --network sepolia
+
+https://sepolia.etherscan.io/address/0xa9e4f211D1630Ee13C2E00a910C5E592DFB4Fb7b#readContract
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,18 @@
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable, defineConfig } from "hardhat/config";
 
+import envEnc from "@chainlink/env-enc";
+envEnc.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
+
+if (!SEPOLIA_RPC_URL || !SEPOLIA_PRIVATE_KEY) {
+  throw new Error(
+    "缺失环境变量！请执行 npx env-enc set 配置 SEPOLIA_RPC_URL、SEPOLIA_PRIVATE_KEY"
+  );
+}
+
 export default defineConfig({
   plugins: [hardhatToolboxViemPlugin],
   solidity: {
@@ -31,8 +43,8 @@ export default defineConfig({
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: SEPOLIA_RPC_URL!,
+      accounts: [SEPOLIA_PRIVATE_KEY!],
     },
   },
 });
